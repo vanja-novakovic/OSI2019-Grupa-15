d15fbfaf65dc1197115014dd094b90c04fa55bbc
Vanja Novakovic <vanja.novakovicvn@gmail.com>
2020-01-02 20:12:17 +0100

    Add table-mapped classes for quiz logic

---
+++ Database/Entities/Answer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Entities
+{
+    public class Answer : IUniquelyIdentifiable, IDbTableAssociate
+    {
+        public int IdAnswer { get; set; }
+
+        public string Content { get; set; }
+
+        public byte True { get; set; }
+
+        public int IdQuestion { get; set; }
+
+        public string GetAssociatedDbTableName()
+        {
+            return "Answer";
+        }
+
+        public string[] GetPrimaryKeyPropertyNames()
+        {
+            return new string[] { "IdAnswer" };
+        }
+    }
+}
---
+++ Database/Entities/Question.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Entities
+{
+    public class Question: IDbTableAssociate, IUniquelyIdentifiable
+    {
+        public int IdQuestion { get; set; }
+        public string Content { get; set; }
+
+        public string GetAssociatedDbTableName()
+        {
+            return "Question";
+        }
+
+        public string[] GetPrimaryKeyPropertyNames()
+        {
+            return new string[] { "IdQuestion" };
+        }
+    }
+}
