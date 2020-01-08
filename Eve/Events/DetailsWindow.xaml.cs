c3b6fa6becfd133c32084f767eb107be6617a44c
Vanja Novakovic <vanja.novakovicvn@gmail.com>
2020-01-08 14:58:47 +0100

    Remove usings

--- Eve/Events/DetailsWindow.xaml.cs
+++ Eve/Events/DetailsWindow.xaml.cs
@@ -1,21 +1,10 @@
 ﻿using Core.Common;
 using Core.Services.Interfaces;
 using Database.Entities;
-using Eve.AutoMapper;
 using Eve.ViewModel;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Eve.Events
 {
